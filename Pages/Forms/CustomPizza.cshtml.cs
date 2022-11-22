b0aee08cb257a50dc062a7c6c76ab90f263d6e87
Jakub <Jakub.klik6@gmail.com>
2022-11-22 08:38:40 +0100

    pizza model, binding

--- Pages/Forms/CustomPizza.cshtml.cs
+++ Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaShop.Models;
 
 namespace PizzaShop.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        //Bind PizzaModel to CustomPizzaModel
+        [BindProperty]
+        //This will allow us to create pizza in CustomPizza.html 
+        public PizzasModel Pizza { get; set; }
+
         public void OnGet()
         {
         }
