56d82baf523a2d12f62d507ab2a1bc7cb59a993d
Jakub <Jakub.klik6@gmail.com>
2022-12-01 11:38:08 +0100

    set up database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaShop.Models;
+
+namespace PizzaShop.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using PizzaShop.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
